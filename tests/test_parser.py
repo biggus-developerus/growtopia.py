03705eb6541c3ebdc4dee443fe8a44657cfb53b6
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2023-04-17 23:21:28 +0400

    add support for items.dat v15

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 """Test the items.dat and player_tribute.dat file parsers."""
 
+import os
 
 from growtopia import ItemsData, PlayerTribute
 
@@ -7,20 +8,23 @@ from growtopia import ItemsData, PlayerTribute
 def test_parser() -> None:
     """Test the parser."""
 
-    items_data = ItemsData("tests/data/items_v14.dat")
+    for file in os.listdir("tests/data"):
+        if file.startswith("items") and file.endswith(".dat"):
+            items_data = ItemsData(f"tests/data/{file}")
+
+            items_data.parse()
+
+            assert items_data.get_item(item_id=2).name.lower() == "dirt"
+            assert items_data.get_item(item_id=3).name.lower() == "dirt seed"
+            assert items_data.get_item(name="blank").id == 0
+            assert items_data.get_item(name="blue gem lock").id == 7188
+
+            assert items_data.get_starts_with("dirt")[0].name.lower() == "dirt"
+            assert items_data.get_ends_with("dirt seed")[0].id == 3
+            assert len(items_data.get_contains("dirt")) > 0
+
     player_tribute = PlayerTribute("tests/data/player_tribute.dat")
-
-    items_data.parse()
     player_tribute.parse()
-
-    assert items_data.get_item(item_id=2).name.lower() == "dirt"
-    assert items_data.get_item(item_id=3).name.lower() == "dirt seed"
-    assert items_data.get_item(name="blank").id == 0
-    assert items_data.get_item(name="blue gem lock").id == 7188
-
-    assert items_data.get_starts_with("dirt")[0].name.lower() == "dirt"
-    assert items_data.get_ends_with("dirt seed")[0].id == 3
-    assert len(items_data.get_contains("dirt")) > 0
 
 
 if __name__ == "__main__":
