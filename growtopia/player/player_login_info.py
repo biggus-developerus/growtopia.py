e8d10ad80d32258b336463ee4a7f15d1a219dbcf
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2023-06-22 15:21:59 +0400

    add missing login packet fields

--- growtopia/player/player_login_info.py
+++ growtopia/player/player_login_info.py
@@ -2,7 +2,35 @@ __all__ = ("PlayerLoginInfo",)
 
 
 class PlayerLoginInfo:
-    def __init__(self) -> None:
+    def __init__(self, kvps: dict[str, str] = {}) -> None:
         self.requested_name: str = ""
         self.tank_id_name: str = ""
         self.tank_id_pass: str = ""
+        self.f: str = ""
+        self.protocol: str = ""
+        self.game_version: str = ""
+        self.fz: str = ""
+        self.lmode: str = ""
+        self.cbits: str = ""
+        self.player_age: str = ""
+        self.gdpr: str = ""
+        self.category: str = ""
+        self.total_playtime: str = ""
+        self.klv: str = ""
+        self.hash2: str = ""
+        self.meta: str = ""
+        self.fhash: str = ""
+        self.rid: str = ""
+        self.platform_id: str = ""
+        self.device_version: str = ""
+        self.country: str = ""
+        self.hash: str = ""
+        self.mac: str = ""
+        self.wk: str = ""
+        self.zf: str = ""
+
+        self.set_attrs(kvps)
+
+    def set_attrs(self, kvps: dict[str, str]) -> None:
+        for key, value in kvps.items():
+            setattr(self, key, value)
