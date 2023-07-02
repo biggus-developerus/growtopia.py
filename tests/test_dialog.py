3cc5a7072c462a243bab2af4140a96dc924f0534
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2023-07-02 04:11:17 +0400

    update test to pass with latest changes

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,20 +1,22 @@
 """Test the Dialog class."""
 
-from growtopia import Dialog
+from growtopia import Dialog, DialogElement
 
 
 def test_dialog() -> None:
     """Hello Dialog"""
 
-    hello = Dialog()
+    hello = Dialog("test_dialog")
 
-    hello.add_textbox("Hello Player!")
-    hello.add_checkbox("test_checkbox", "Do you want to play?", False)
-    hello.add_ending("test_dialog", "Back", "Enter")
+    hello.add_elements(
+        DialogElement.textbox("Hello Player!"),
+        DialogElement.checkbox("test_checkbox", "Do you want to play?", False),
+        DialogElement.ending("test_dialog", "Back", "Enter"),
+    )
 
     assert (
         hello.dialog
-        == "add_textbox|Hello Player!|left|\nadd_checkbox|test_checkbox|Do you want to play?|0|\nend_dialog|test_dialog|Back|Enter|\n"
+        == "add_textbox|Hello Player!|left|\nadd_checkbox|test_checkbox|Do you want to play?|0|\nend_dialog|test_dialog|Back|Enter"
     )
 
 
