e96c0c5bd1535d9110362e8b8bd38c06d3bc150b
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2023-07-19 22:34:06 +0400

    update docstr

--- growtopia/player/player.py
+++ growtopia/player/player.py
@@ -19,9 +19,9 @@ class Player(PlayerNet):
     ----------
     peer: enet.Peer
         The peer object of the player.
-    last_packet_sent: None
+    last_packet_sent: Union[StrPacket, GameUpdatePacket]
         The last packet that was sent to the player.
-    last_packet_received: None
+    last_packet_received: Union[StrPacket, GameUpdatePacket]
         The last packet that was received from the player.
     login_info: PlayerLoginInfo
         A PlayerLoginInfo object that contains the data that the player sent when they logged in. (e.g. username, password, etc.)
