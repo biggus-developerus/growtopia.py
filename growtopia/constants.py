734f0ddac62cce683b7be9aa0443e04f5cb8bd6b
kajob-dev <theamazingtootle@gmail.com>
2023-12-26 21:29:30 +0400

    oopsie

--- growtopia/constants.py
+++ growtopia/constants.py
@@ -26,10 +26,10 @@ Example:
 """
 
 ignored_attributes = {
-    11: ["flags3", "bodypart", "flags4", "flags5", "unknown", "sit_file"],
-    12: ["flags4", "flags5", "unknown", "sit_file"],
-    13: ["flags5", "unknown", "sit_file"],
-    14: ["unknown", "sit_file"],
+    11: ["flags3", "bodypart", "flags4", "flags5", "unknown", "sit_file", "renderer"],
+    12: ["flags4", "flags5", "unknown", "sit_file", "renderer"],
+    13: ["flags5", "unknown", "sit_file", "renderer"],
+    14: ["unknown", "sit_file", "renderer"],
     15: ["renderer"],
     16: [],
 }
