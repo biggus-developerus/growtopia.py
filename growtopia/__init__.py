c1ebca53275754e17739eaad456620475358df8a
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2024-01-21 13:28:02 +0400

    Update __init__.py

--- growtopia/__init__.py
+++ growtopia/__init__.py
@@ -1,7 +1,5 @@
 from .parsers import *
 from .utils import *
 
-if not Logger.start():
+if not Logger._running and not Logger.start():
     print(AnsiStr("Failed to start logger").wrap(AnsiESC.RED))
-else:
-    log_info("Logger started")
