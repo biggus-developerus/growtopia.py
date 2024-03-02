9711f327d195abea2bc8d8d7187d732f98d79014
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2024-03-02 12:22:07 +0400

    Update __main__.py

--- growtopia/__main__.py
+++ growtopia/__main__.py
@@ -1,28 +1,11 @@
-from typing import Union
-
 from growtopia import (
-    File,
     ItemsData,
-    Logger,
-    PlayerTribute,
 )
 
 
 def parse_file(path: str) -> None:
-    data: Union[ItemsData, PlayerTribute, None] = None
+    items_data = ItemsData.load(path)
 
-    if File.is_items_data(path):
-        data = ItemsData(path)
-    # elif File.is_player_tribute(path):
-    #     data = PlayerTribute(path)
-
-    if data is None:
-        raise ValueError("Unknown file type, must be items.dat or player_tribute.dat")
-
-    data.parse()
-
-    if isinstance(data, PlayerTribute):
-        return
 
 def main(*args):
     tool = args[0] if args else None
