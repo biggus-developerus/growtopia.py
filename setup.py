7da0d0e913dfee7b657c033cacb2c84ad64b5792
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2024-03-03 17:12:56 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=[
         path.join(root).replace("\\", ".")
         for root, _, files in walk("growtopia")
-        if "__init__.py" in files and root != "stubs"
+        if "__init__.py" in files
     ],
 )
