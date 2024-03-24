60c04d185feee2f93fe6dbb6d2f182d5284cbedd
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2024-03-24 15:55:23 +0400

    Update __init__.py

--- growtopia/utils/__init__.py
+++ growtopia/utils/__init__.py
@@ -1,5 +1,19 @@
-from .ansi import *
+# isort:skip_file
+from logging import (
+    DEBUG as LOG_LEVEL_DEBUG,
+    INFO as LOG_LEVEL_INFO,
+    WARNING as LOG_LEVEL_WARNING,
+    ERROR as LOG_LEVEL_ERROR,
+    CRITICAL as LOG_LEVEL_CRITICAL,
+)
+
 from .buffer import *
 from .compression import *
 from .crypto import *
-from .logger import *
+from .setup import *
+
+logger = Setup.setup_logger("growtopia")
+
+
+def log(level: int, msg: str) -> None:
+    logger.log(level, msg)
