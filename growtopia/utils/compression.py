5b30451c22ab6d67dc1c3055eabe2b02e39fb247
kaJob-dev <74741815+kaJob-dev@users.noreply.github.com>
2024-03-24 15:55:17 +0400

    Update compression.py

--- growtopia/utils/compression.py
+++ growtopia/utils/compression.py
@@ -1,7 +1,7 @@
 __all__ = ("zlib_compress", "zlib_decompress", "CompressionType")
 
-
 from enum import Enum
+from typing import Union
 from zlib import (
     Z_BEST_COMPRESSION,
     compress,
@@ -18,10 +18,10 @@ class CompressionType(Enum):
 
 
 @typechecked
-def zlib_compress(data: bytearray) -> bytes:
-    return compress(data, level=Z_BEST_COMPRESSION)
+def zlib_compress(data: Union[memoryview, bytearray], level: int = Z_BEST_COMPRESSION) -> bytearray:
+    return bytearray(compress(data, level=level))
 
 
 @typechecked
-def zlib_decompress(data: bytearray) -> bytes:
-    return decompress(data)
+def zlib_decompress(data: Union[memoryview, bytearray]) -> bytearray:
+    return bytearray(decompress(data))
