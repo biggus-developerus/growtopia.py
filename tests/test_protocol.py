e76f3e156209b364fbc4753a7f931ded16b224c5
kajob-dev <xd@xd.com>
2024-04-20 15:09:02 +0400

    xd

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,4 +1,3 @@
-import pytest
 import growtopia
 
 from os import chdir, path
@@ -8,7 +7,7 @@ chdir(path.abspath(path.dirname(__file__)))
     
 def test_protocol() -> None:
     packet = growtopia.Packet(growtopia.PacketType.HELLO)
-    assert packet.pack() == bytearray([1,0,0,0])
+    assert (data:=packet.pack()) and data == bytearray([1,0,0,0])
 
 if __name__ == "__main__":
     test_protocol()
